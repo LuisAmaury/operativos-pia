52eead95ba631933ac5fe414feb2eb7df6545e32
Skope10 <ferbarrera_fraga@outlook.es>
2018-04-19 18:56:42 -0500

    Agregando User - Group - Student

---
+++ app/src/main/java/com/example/luisamaury/operativos_pia/AddGroupActivity.java
@@ -0,0 +1,213 @@
+package com.example.luisamaury.operativos_pia;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class AddGroupActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    Spinner spinner, spinner2;
+    String nameSubject, subID, idSubject, nameHour, idHour, fit;
+    Button btnViewSubjectID;
+    EditText editFit;
+    View viewer;
+    Integer namePosition;
+
+    MyDBHandler db = new MyDBHandler(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_group);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+        editFit = (EditText) findViewById(R.id.editText_Fit);
+        btnViewSubjectID = (Button)findViewById(R.id.btnViewID);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            if(bundle.getString("some") != null){
+                Toast.makeText(getApplicationContext(),
+                        "data: "+ bundle.getString("some"),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        loadSpinnerData(); //for Subjects
+        loadSpinnerData2(); //for Schedule
+
+        spinner.setOnItemSelectedListener(this);
+        spinner2.setOnItemSelectedListener(this);
+
+
+        clickBtn();
+
+    }
+    private void loadSpinnerData() {
+        // database handler
+        MyDBHandler db = new MyDBHandler(getApplicationContext());
+
+        // Spinner Drop down elements
+        List<String> lables = db.getAllSubjects();
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, lables);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinner.setAdapter(dataAdapter);
+    }
+
+    private void loadSpinnerData2(){
+        // database handler
+        MyDBHandler db = new MyDBHandler(getApplicationContext());
+
+        // Spinner Drop down elements
+        List<String> schedules = db.getAllSchedules();
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, schedules);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter
+                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        spinner2.setAdapter(dataAdapter);
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position,
+                               long id) {
+        Spinner spinnerCheck = (Spinner) parent;
+
+        if(spinnerCheck.getId() == R.id.spinner)
+        {
+            String label = parent.getItemAtPosition(position).toString();
+            namePosition = position;
+            nameSubject = label;
+
+            // Showing selected spinner item
+            //Toast.makeText(parent.getContext(), "You selected : " + label,
+            //      Toast.LENGTH_SHORT).show();
+            Spinner spinner = (Spinner) parent;
+
+        }
+        else if(spinnerCheck.getId() == R.id.spinner2)
+        {
+            String label = parent.getItemAtPosition(position).toString();
+
+            nameHour = label;
+
+            // Showing selected spinner item
+            //Toast.makeText(parent.getContext(), "You selected : " + label,
+            //Toast.LENGTH_SHORT).show();
+            Spinner spinner = (Spinner) parent;
+        }
+
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+    public void clickBtn(){
+        //MyDBHandler MYdb = new MyDBHandler(getApplicationContext());
+
+
+        //idSubject = aid.getString(0);
+        btnViewSubjectID.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor aid = db.getSubjectID(nameSubject);
+                        StringBuffer buffer = new StringBuffer();
+                        Cursor spin2 = db.getScheduleId(nameHour);
+
+                        try {
+
+                            while (aid.moveToNext()) { // puesto a que comienza en -1, debemos de pasarlo al siguiente, y se supone que solo debe tener 1 valor
+                                idSubject = aid.getString(0);
+                                // buffer.append("ID Subject :"+ idSubject+"\n");
+                            }
+                            while (spin2.moveToNext()) { // puesto a que comienza en -1, debemos de pasarlo al siguiente, y se supone que solo debe tener 1 valor
+                                idHour = spin2.getString(0);
+                                // buffer.append("ID Hour :"+ idHour+"\n");
+                            }
+                        }catch(Exception e){
+                            e.getCause();
+                            Toast.makeText(AddGroupActivity.this," Error! ",Toast.LENGTH_SHORT).show();
+
+                        }
+                        //VALIDATE
+                        if(!validate()){
+                            Toast.makeText(AddGroupActivity.this," Error! ",Toast.LENGTH_SHORT).show();
+                        }else{
+                            saveNewGroup();
+                        }
+
+                        // Show all data
+                        //showMessage("Data",buffer.toString());
+
+                    }
+                });
+    }
+
+    public void showMessage(String title,String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+
+    public boolean validate(){
+        boolean valid = true;
+        fit =editFit.getText().toString().trim();
+
+        // Toast.makeText(AddGroupActivity.this,"Fit: "+fit,Toast.LENGTH_SHORT).show();
+
+        if (fit.isEmpty() )
+        {
+            editFit.setError("Porfavor llena el Cupo");
+            valid = false;
+        }
+
+        return valid;
+    }
+    public void saveNewGroup(){
+        //Toast.makeText(AddGroupActivity.this," WORK WORK! ",Toast.LENGTH_SHORT).show();
+
+        boolean isInserted = db.saveNewGroup(idSubject,idHour,fit);
+        if(isInserted == true)
+            Toast.makeText(AddGroupActivity.this,"Informacion Ingresada",Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(AddGroupActivity.this,"Informacion no Ingresada",Toast.LENGTH_SHORT).show();
+        editFit.setText("");
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/luisamaury/operativos_pia/AddStudentActivity.java
@@ -0,0 +1,89 @@
+package com.example.luisamaury.operativos_pia;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddStudentActivity extends AppCompatActivity {
+
+
+    String tryout;
+    MyDBHandler myDb;                           // Base de datos
+    EditText editName,editTextId, editPhone, editIdUsuario;            // campos de texto
+    Button btnAddData;                         // Botones
+    Button btnviewAll;
+
+
+    Button btnviewUpdate;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_student);
+        myDb = new MyDBHandler(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //editName = (EditText)findViewById(R.id.editText_nameAlumno);
+        editName = (EditText)findViewById(R.id.editText_AddStudentName);
+        editPhone = (EditText)findViewById(R.id.editText_AddStudentPhone);
+        editIdUsuario = (EditText)findViewById(R.id.editText_AddStudentID);
+
+        btnAddData = (Button)findViewById(R.id.btnAddNewStudent);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        AddData();
+
+    }
+
+
+    public  void AddData() {
+        btnAddData.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        /*try {
+
+                            tryout =editName.getText().toString().trim();
+                            Toast.makeText(AddStudentActivity.this,"WORKS? "+tryout,Toast.LENGTH_LONG).show();
+                        }catch(Exception e){
+                            e.getCause();
+                            //Toast.makeText(AddStudentActivity.this," Error! ",Toast.LENGTH_SHORT).show();
+
+                        }*/
+
+                        // Toast.makeText(AddGroupActivity.this,"Fit: "+fit,Toast.LENGTH_SHORT).show();
+
+                        boolean isInserted = myDb.insertDataAlumno(editName.getText().toString(), editPhone.getText().toString(), Integer.parseInt(editIdUsuario.getText().toString()));
+                        if(isInserted == true)
+                            Toast.makeText(AddStudentActivity.this,"Informacion Ingresada",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(AddStudentActivity.this,"Informacion no Ingresada",Toast.LENGTH_LONG).show();
+                        editName.setText("");
+
+                        editPhone.setText("");
+                        editIdUsuario.setText("");
+                    }
+                }
+        );
+    }
+
+
+
+    public void showMessage(String title,String Message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(Message);
+        builder.show();
+    }
+}
