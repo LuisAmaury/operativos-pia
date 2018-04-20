311f3631b97b996b08856d5f1d6813c05382a643
Carlos García <jcharliegmontes@gmail.com>
2018-04-20 04:51:59 -0500

    Inscriptions
    Separate inscriptions fragments admin/alumni

---
+++ app/src/main/java/com/example/luisamaury/operativos_pia/inscripcion/AddInscriptionStudentActivity.java
@@ -0,0 +1,11 @@
+package com.example.luisamaury.operativos_pia.inscripcion;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jchar on 20/04/2018.
+ */
+
+public class AddInscriptionStudentActivity extends AppCompatActivity {
+
+}
