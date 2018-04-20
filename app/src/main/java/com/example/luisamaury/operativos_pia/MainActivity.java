3f9c68df3af6e3063d908b227fb9cfbf44300657
Carlos García <jcharliegmontes@gmail.com>
2018-04-19 22:01:40 -0500

    Complete LogIn Funtionality

--- app/src/main/java/com/example/luisamaury/operativos_pia/MainActivity.java
+++ app/src/main/java/com/example/luisamaury/operativos_pia/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.luisamaury.operativos_pia;
 
 
-import android.database.Cursor;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 
 public class MainActivity extends AppCompatActivity {
     final Context context = this;
@@ -33,29 +31,7 @@ public class MainActivity extends AppCompatActivity {
                 String password = passwordEditText.getText().toString();
 
                 if(localDatabase.loginCheck(user, password)){
-                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                            context);
-
-                    // set title
-                    alertDialogBuilder.setTitle("Your Title");
-
-                    // set dialog message
-                    alertDialogBuilder
-                            .setMessage(user + "" + password)
-                            .setCancelable(false)
-                            .setNegativeButton("No",new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
-                                    // if this button is clicked, just close
-                                    // the dialog box and do nothing
-                                    dialog.cancel();
-                                }
-                            });
-
-                    // create alert dialog
-                    AlertDialog alertDialog = alertDialogBuilder.create();
-
-                    // show it
-                    alertDialog.show();
+                    startActivity(new Intent(MainActivity.this,MenuActivity.class));
                  }
             }
         });
